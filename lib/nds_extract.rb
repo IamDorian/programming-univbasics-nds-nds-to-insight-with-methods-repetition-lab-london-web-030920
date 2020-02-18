62c0575c87f6a1bec16f8e018edf9e7075ce14ef
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-18 16:42:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -44,14 +44,14 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  total = 0
-  index = 0
-  binding.pry
-  while index < source[:movies].length do
-    total += source[:movies][index][:worldwide_gross]
-    index += 1
+  result = {}
+  director_index = 0
+  while director_index < source.size do
+    director = source[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
   end
-total
+result
 end
 
 
