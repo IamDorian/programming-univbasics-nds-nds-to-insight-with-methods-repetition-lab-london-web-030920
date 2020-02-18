57c4229a34be049bea45c264bd26fe5ec0d45179
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-18 17:38:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,6 +45,15 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  dir_name = directors_totals(source)
+  list = list_of_directors(source)
+  total = 0 
+  i = 0
+  
+  while i < list.length do 
+      name = list[i] 
+      binding.pry
+  end
   
 end
 
